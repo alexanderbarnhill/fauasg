b0b754f753e02c8baf7ca26f5ab9838b5ee543ee
Alex <alex@alexanderbarnhill.com>
2024-01-12 21:33:08 +0100

    Add further setup tools

--- fauasg/__init__.py
+++ fauasg/__init__.py
@@ -3,6 +3,6 @@ fauasg.
 
 """
 
-__version__ = "0.1.0"
+__version__ = "0.0.2"
 __author__ = 'Alexander Barnhill'
 __credits__ = 'Friedrich-Alexander-Universität Erlangen-Nürnberg Pattern Recognition Lab'
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='fauasg',
-    version='0.1.0',
+    version='0.0.2',
     description='A set of tools developed by the Animal Speech Group at FAU',
     url='https://github.com/alexanderbarnhill/fauasg',
     author='Alexander Barnhill',
     author_email='alexander.barnhill@fau.de',
     license='MIT',
-    packages=['fauasg'],
+    packages=find_packages(),
     install_requires=['torch',
                       'numpy',
                       'matplotlib',
